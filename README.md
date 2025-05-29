ba4cd2fe4dd7a913f0d18c40bddd1d537d3d3942
darharel <darharel@gmail.com>
2025-05-29 15:24:03 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jenkins Demo Flask App
+
+This is a simple Flask app to demonstrate CI/CD with Jenkins.
---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from app import create_app
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.testing = True
+    return app.test_client()
+
+def test_home(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello from Flask App for Jenkins Demo!" in response.data
